c4bf4d18a00c3a72d9d2b119123d3866ac44f686
djshiesty <soni20283106@student.nhvweb.net>
2026-09-10 13:08:40 +0000

    adds oops

---
+++ BasicOop.java
@@ -0,0 +1,16 @@
+public class BasicOop {
+    public static void main(String[] args) {
+        // attributes / data
+        // variables
+        
+
+        // Constructor --- SPECIAL METHOD called every time you make an object.
+        // Same name as the class, followed by parentheses.
+        // BasicOop()
+
+
+        // Behaviors / methods
+        // you can write as many as you want
+
+    }
+}
